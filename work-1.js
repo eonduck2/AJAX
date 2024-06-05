a77f03f11d0921823dc383c4ac0a9fb1ef0f659f
eonduck2 <whdtnxd@gmail.com>
2024-06-05 12:02:53 +0900

    update 'work-1.js'

--- work-1.js
+++ work-1.js
@@ -10,10 +10,6 @@ const endPoint = `http://apis.data.go.kr/6300000/eventDataService`;
 
 const myKey = `4zYfAHQ%2Bxd9VHwGpOHJ94w9GtY7b9QSyfS6P8utonBCXN%2BjTsrgkaPR9EGAbukX5f7fAuLBjtGxaZYSet8JJVQ%3D%3D`;
 
-const eg = `http://apis.data.go.kr/6300000/eventDataService/eventDataListJson
-?
-serviceKey
-=
-${myKey}&numOfRows=10&pageNo=1`;
+const eg = `http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=${myKey}&numOfRows=40&pageNo=1`;
 
 console.log(eg);
