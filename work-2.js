942b5b6c7967807085248aab3dff479a0169557b
eonduck2 <whdtnxd@gmail.com>
2024-06-05 12:05:05 +0900

    create a new file 'work-2.js'

---
+++ work-2.js
@@ -0,0 +1,5 @@
+const myKey = `4zYfAHQ%2Bxd9VHwGpOHJ94w9GtY7b9QSyfS6P8utonBCXN%2BjTsrgkaPR9EGAbukX5f7fAuLBjtGxaZYSet8JJVQ%3D%3D`;
+
+const eg = `http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?serviceKey=${myKey}&numOfRows=10&pageNo=1`;
+
+console.log(eg);
