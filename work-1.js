8488b07e3a40fda3d3ab80607a60b682bd3e6cf1
eonduck2 <whdtnxd@gmail.com>
2024-06-05 11:55:10 +0900

    update 'work-1.js'

---
+++ work-1.js
@@ -0,0 +1,19 @@
+const firstUrl = `https://apis.data.go.kr/6300000/eventDataService/eventDataListJson
+?
+serviceKey
+=
+4zYfAHQ%2Bxd9VHwGpOHJ94w9GtY7b9QSyfS6P8utonBCXN%2BjTsrgkaPR9EGAbukX5f7fAuLBjtGxaZYSet8JJVQ%3D%3D&`;
+
+const secondUrl = `end point 주소 + 주소 값`;
+
+const endPoint = `http://apis.data.go.kr/6300000/eventDataService`;
+
+const myKey = `4zYfAHQ%2Bxd9VHwGpOHJ94w9GtY7b9QSyfS6P8utonBCXN%2BjTsrgkaPR9EGAbukX5f7fAuLBjtGxaZYSet8JJVQ%3D%3D`;
+
+const eg = `http://apis.data.go.kr/6300000/eventDataService/eventDataListJson
+?
+serviceKey
+=
+${myKey}&numOfRows=10&pageNo=1`;
+
+console.log(eg);
